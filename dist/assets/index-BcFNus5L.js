926d6abca90fe84b01114a9c55f068a94d242735
I-am-shy <119798746+I-am-shy@users.noreply.github.com>
2024-06-25 23:50:27 +0800

    Delete dist directory

--- dist/assets/index-BcFNus5L.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,Z as t,Y as _}from"./index-BZ2uke3n.js";const a={__name:"index",setup(c){return(n,o)=>(_(),t("div",null,"登录页"))}},r=e(a,[["__scopeId","data-v-846e49d3"]]);export{r as default};

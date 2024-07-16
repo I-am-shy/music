53d6bf196560e73b18de97647e9d67a79570932d
I-am-shy <2050808169@qq.com>
2024-07-16 21:02:24 +0800

    pc端优化

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,5 +1,6 @@
 // 获取assets静态资源
 export default  function (name){
+  // 参数，文件名称，自动定位到 assets文件夹下
   const url = new URL(`../assets/${name}`, import.meta.url).href
   // console.log(url)
   return url;
